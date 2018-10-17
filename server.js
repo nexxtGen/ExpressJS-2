d7eed7bf58232cda06d424d508a9bf8f0453cb04
Kamil Szczypior <kszczypior1@gmail.com>
2018-10-17 10:25:24 +0200

    17-6 Pug

--- server.js
+++ server.js
@@ -2,28 +2,41 @@
 var express = require('express');
 var app = express();
 var path = require('path')
-var fs = require('fs');
 
 app.set('view engine', 'pug'); //mówi , że będziemy używać Puga jako kreatora widoków
 app.set('views','./views'); // mowi , że widoki (views) będziemy trzymać w katalogu /views.
 
 // App section
-app.use('/store', function(req, res, next) {
-    console.log('Jestem pośrednikiem przy żądaniu do /store');
+app.use('/', function(req, res, next) {
+    console.log('Sprawdzam czy user jest zalogowany');
+    next();
+});
+app.use('/', function(req, res, next) {
+    console.log('Jeśli tak- Sprawdzam jego poziom uprawnień i Przekierowywuję do panelu usera. Jeśli nie- wyswietlam stronę główną z form do zalogowania');
     next();
 });
 
 app.get('/', function (req, res) {
-    res.send('Hello world!');
+    res.render('index')
 });
 
-app.get('/store', function (req, res) {
-    res.send('To jest sklep');
+app.get('/auth/snowflakes.jpg', function (req, res) {
+    const image = path.join(__dirname, 'views', 'snowflakes.jpg');
+    res.sendFile(image);  
 });
 
-
-
-
+app.get('/auth/google', function (req, res) {
+    console.log('hasło: ', req.query)
+    if (req.query.password != '') {
+        res.render('user-panel', {
+            email: req.query.email,
+            pass: req.query.password
+        });
+    } else {
+        res.send('Wróc i podaj nam swoje hasło.');
+    }
+    
+});
 
 var server = app.listen(3000, 'localhost', function() {
     var host = server.address().address;
